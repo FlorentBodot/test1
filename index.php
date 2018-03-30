307b29d552734be40c984b9a6c96d4764d5ea820
JeremyDilard <35807152+JeremyDilard@users.noreply.github.com>
2018-03-30 15:29:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Docounet</title>
 </head>
 <body>
     <div>
-        <h1>Coucou</h1>
+        <h1>Salutounet</h1>
     </div>
+    carousel(mdr 'zigounoune')
 </body>
 </html>
